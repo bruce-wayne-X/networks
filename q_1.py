eb66488fca9643169b54afbd6b4974d10b261a07
bruce-wayne-X <anujgita@gmail.com>
2017-10-16 17:55:53 +0530

    Code before Reply

--- q_1.py
+++ q_1.py
@@ -1,5 +1,8 @@
 import csv
 from collections import deque
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import math
 class Device:
 	def __init__(self,id,discoverer_id):
 		self.id=id
@@ -16,20 +19,6 @@ class Device:
 	def __eq__(self,other):
 		return self.id==other.id
 
-
-# def Search_Recursion(id,Global_num,K):
-# 	it=0
-# 	queue=[]
-# 	for i in Device_list_dict[id]:
-# 		if(Device_list_dict[i].recChunk==False & it<K):
-# 			Device_list_dict[i].recChunk=True
-# 			Global_num+=1
-# 			it+=1
-# 			queue.append(i)
-# 	for i in queue:			
-# 		if(Global_num<0.9*len(Device_list_dict)):
-# 			Search_Recursion(i,Global_num)
-
 Global_num=0
 Time_Broadcast=0
 def Broadcaster(Starter_id,K):
@@ -43,11 +32,11 @@ def Broadcaster(Starter_id,K):
 	queue.append(Starter_id)
 	Device_list_dict[Starter_id].recChunk=True
 	Global_num+=1
-	# print(len(queue))
-	# print(int(0.9*len(Device_list_dict)))
-	# print(Global_num<int(0.9*len(Device_list_dict)) )
-	# print(len(queue)>0)
-	# print(Global_num<int(0.9*len(Device_list_dict)) & len(queue)>0)
+		# print(len(queue))
+		# print(int(0.9*len(Device_list_dict)))
+		# print(Global_num<int(0.9*len(Device_list_dict)) )
+		# print(len(queue)>0)
+		# print(Global_num<int(0.9*len(Device_list_dict)) & len(queue)>0)
 	while ((Global_num<int(0.9*len(Device_list_dict))) & (len(queue)>0)):
 		# print("haha")
 		first_=queue.popleft()
@@ -84,21 +73,38 @@ with open('proximityedgestimestamps.csv') as csvfile:
 			if (int(row[2]) not in Device_list_dict):
 				New_device=Device(int(row[2]),int(row[1]),int(row[0]))
 				Device_list_dict[int(row[2])]=New_device
-			# 	Device_list_dict[int(row[2])].node_set.add(int(row[1]))
-			# else:
-			# 	Device_list_dict[int(row[2])].node_set.add(int(row[1]))
-i=76
-# while (Global_num<int(0.9*len(Device_list_dict))):
-# 	Broadcaster(26,i)
-# 	print(str(i)+" "+str(Global_num)+" "+str(Time_Broadcast))
-# 	i+=1
-Broadcaster(26,2)
-while (i<5000):
-	try:
-		if((Device_list_dict[i].node_set)!=[]):
-			print(Device_list_dict[i].node_set)
-			print(i)
-	except:
-		pass
-	
+				Device_list_dict[int(row[2])].node_set.add(int(row[1]))
+				else:
+				if int(row[1]) not in Device_list_dict[int(row[2])].node_set:
+					Device_list_dict[int(row[1])].node_set.append(int(row[1]))
+
+i=1
+while (Global_num<int(0.9*len(Device_list_dict))):
+	Broadcaster(26,i)
+	print(str(i)+" "+str(Global_num)+" "+str(Time_Broadcast))
 	i+=1
+	Broadcaster(26,2)
+	# while (i<5000):
+	# 	try:
+	# 		if((Device_list_dict[i].node_set)!=[]):
+	# 			print(Device_list_dict[i].node_set)
+	# 			print(i)
+	# 	except:
+	# 		pass
+		
+	# 	i+=1
+
+Device_list_list=list(Device_list_dict.values())
+Device_list_list=sorted(Device_list_list,key=lambda Device: len(Device.node_set),reverse=True)
+Device_list_x=[]
+Device_list_y=[]
+k=0
+for i in range(0,len(Device_list_list)-1):
+	k+=len(Device_list_list[i].node_set)
+	Device_list_x.append(math.log(i+1))
+	Device_list_y.append(k)
+plt.plot(Device_list_x,Device_list_y)
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.annotate()
+plt.show()
